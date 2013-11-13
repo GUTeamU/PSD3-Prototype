5987b349b129adc999cbfe5a926a59120e437726
Michael W Cromie <michaelcromie22@gmail.com>
2013-11-13 13:49:17 +0000

    Copying prototype repo to new repo

---
+++ Prototype.py
@@ -0,0 +1,12 @@
+import HelperFunctions.py
+
+# Get user login
+
+# Import the sessions
+# Present the user with the sessions
+
+# Get user input for selecting the session
+# Process appropriately, eg. for a recurring task prompt the user to select a specific session
+
+# Present the user with list of options for selected session
+# Get users input and process appropriately
