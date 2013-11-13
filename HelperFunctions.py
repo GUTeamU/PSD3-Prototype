583bb8649ef1637c18df874aab84f29f504b1393
Andrew McDonald <mcdonalda1993@gmail.com>
2013-11-13 18:44:38 +0000

    db tables seems to be implemented correctly

--- HelperFunctions.py
+++ HelperFunctions.py
@@ -8,10 +8,20 @@ def init_db():
 	print glob.glob("*.csv")
 	db = sqlite3.connect(":memory:")
 	cursor = db.cursor()
-	 f = open('dbSchema.sql', 'r')
+	f = open('dbSchema.sql', 'r')
 	sqlscript = f.read()
 	f.close()
 	cursor.executescript(sqlscript)
+	db.commit()
+	
+	# Was used to test the DB
+	# cursor.execute("PRAGMA table_info(session_types)")
+	# print cursor.fetchall()
+	
+	cursor.close()
+	return db
+
+
 	
 	
 # if __name__ == '__main__':
